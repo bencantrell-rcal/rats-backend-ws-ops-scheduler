c75508540572f8615e32673478d2b814572469af
bencantrell-rcal <ben.cantrell@rcalproducts.com>
2025-09-09 16:57:48 -0500

    Initial commit

---
+++ src/main/java/com/rcal/rcal_ops_scheduler/RcalOpsSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.rcal.rcal_ops_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcalOpsSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcalOpsSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rcal/rcal_ops_scheduler/RcalOpsSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcal.rcal_ops_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcalOpsSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
