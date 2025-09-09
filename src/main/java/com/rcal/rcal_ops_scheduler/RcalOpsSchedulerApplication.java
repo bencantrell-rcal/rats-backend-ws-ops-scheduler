fb8a3d47cd1b25e582a094dac5bde2255f29fb43
bencantrell-rcal <ben.cantrell@rcalproducts.com>
2025-09-09 17:31:19 -0500

    Add workflow files

--- src/main/java/com/rcal/rcal_ops_scheduler/RcalOpsSchedulerApplication.java
+++ src/main/java/com/rcal/rcal_ops_scheduler/RcalOpsSchedulerApplication.java
@@ -2,12 +2,13 @@ package com.rcal.rcal_ops_scheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-public class RcalOpsSchedulerApplication {
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class RcalOpsSchedulerApplication{
 
-	public static void main(String[] args) {
-		SpringApplication.run(RcalOpsSchedulerApplication.class, args);
-	}
+  public static void main(String[] args){
+    SpringApplication.run(RcalOpsSchedulerApplication.class,args);
+  }
 
 }
