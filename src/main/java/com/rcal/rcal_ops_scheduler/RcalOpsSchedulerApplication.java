c4b8b683eade08093a5809e426754344fa8ef3af
bencantrell-rcal <ben.cantrell@rcalproducts.com>
2025-09-16 16:17:33 -0500

    liveness and readiness probe setup

--- src/main/java/com/rcal/rcal_ops_scheduler/RcalOpsSchedulerApplication.java
+++ src/main/java/com/rcal/rcal_ops_scheduler/RcalOpsSchedulerApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class RcalOpsSchedulerApplication{
-
+  // ---------------------------------------------------------------------------
+  // Purpose: This is the main method of the application. When run, the app
+  // runs indefinitely with the endpoints exposed.
+  // ---------------------------------------------------------------------------
   public static void main(String[] args){
     SpringApplication.run(RcalOpsSchedulerApplication.class,args);
   }
--- src/main/java/com/rcal/rcal_ops_scheduler/controller/OpsSchedulerController.java
+++ src/main/java/com/rcal/rcal_ops_scheduler/controller/OpsSchedulerController.java
@@ -8,16 +8,21 @@ import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
 @RestController
-public class OpsSchedulerController {
+public class OpsSchedulerController{
 
-    private static final DateTimeFormatter FORMATTER =
-            DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+  private static final DateTimeFormatter FORMATTER = DateTimeFormatter
+      .ofPattern("yyyy-MM-dd'T'HH:mm:ss");
 
-    @GetMapping("/health")
-    public String health() {
-        String timestamp = LocalDateTime.now(ZoneOffset.UTC).format(FORMATTER);
-        String message = "Healthy " + timestamp;
-        System.out.println(message);
-        return message;
-    }
+  // ---------------------------------------------------------------------------
+  // Purpose: This method serves as a check to verify if the app is exposing
+  // its endpoints correctly. When this endpoint is hit, it responds "healthy"
+  // with a timestamp attached
+  // ---------------------------------------------------------------------------
+  @GetMapping("/health")
+  public String health(){
+    String timestamp = LocalDateTime.now(ZoneOffset.UTC).format(FORMATTER);
+    String message = "Healthy " + timestamp;
+    System.out.println(message);
+    return message;
+  }
 }
