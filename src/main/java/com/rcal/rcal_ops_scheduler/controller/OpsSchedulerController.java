3261a3423a36b12a921b6764d80dba07130f5534
bencantrell-rcal <ben.cantrell@rcalproducts.com>
2025-09-16 10:39:17 -0500

    simplifying endpoint path

--- src/main/java/com/rcal/rcal_ops_scheduler/controller/OpsSchedulerController.java
+++ src/main/java/com/rcal/rcal_ops_scheduler/controller/OpsSchedulerController.java
@@ -13,7 +13,7 @@ public class OpsSchedulerController {
     private static final DateTimeFormatter FORMATTER =
             DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
 
-    @GetMapping("/opsscheduler/health")
+    @GetMapping("/health")
     public String health() {
         String timestamp = LocalDateTime.now(ZoneOffset.UTC).format(FORMATTER);
         String message = "Healthy " + timestamp;
