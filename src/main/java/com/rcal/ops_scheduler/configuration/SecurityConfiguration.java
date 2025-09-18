9f8c06cf4d6f03903dcb7f19a74ce81693a408d3
bencantrell-rcal <ben.cantrell@rcalproducts.com>
2025-09-18 15:19:54 -0500

    documentation

--- src/main/java/com/rcal/ops_scheduler/configuration/SecurityConfiguration.java
+++ src/main/java/com/rcal/ops_scheduler/configuration/SecurityConfiguration.java
@@ -5,14 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
 @Configuration
-public class SecurityConfiguration {
+public class SecurityConfiguration{
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers(
-                "/swagger-ui/**",
-                "/v3/api-docs/**",
-                "/swagger-ui.html"
-        );
-    }
+  // ---------------------------------------------------------------------------
+  // Purpose: This Spring Bean defines endpoints that should be let through
+  // spring security. This specifies to permit the appropriate swagger calls.
+  // ---------------------------------------------------------------------------
+  @Bean
+  public WebSecurityCustomizer webSecurityCustomizer(){
+    return (web) -> web.ignoring().requestMatchers("/swagger-ui/**",
+        "/v3/api-docs/**","/swagger-ui.html");
+  }
 }
