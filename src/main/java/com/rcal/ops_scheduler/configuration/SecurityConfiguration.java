3693b7b0447b3fa03af7ba86a51551d4b4af282a
bencantrell-rcal <ben.cantrell@rcalproducts.com>
2025-09-18 15:14:37 -0500

    Define swagger endpoints as authorized

---
+++ src/main/java/com/rcal/ops_scheduler/configuration/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package com.rcal.ops_scheduler.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(
+                "/swagger-ui/**",
+                "/v3/api-docs/**",
+                "/swagger-ui.html"
+        );
+    }
+}
